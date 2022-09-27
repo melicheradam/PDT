b89cbf38236075e8a3a552c10819f68c10a7aeba
Adam Melicher <addik36@gmail.com>
2022-09-27 22:50:45 +0200

    did some work on conversations uploader

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import asyncio
 import os
-import time
-import zlib
 from datetime import datetime
-from os import read
 import gzip
-import aiofiles
-import ijson
 from orjson import orjson
 import psycopg2
 from psycopg2.extras import execute_values
-
-CHUNKSIZE = 4096
-d = zlib.decompressobj(16+zlib.MAX_WBITS)
 
 
 class Uploader:
@@ -41,6 +32,18 @@ class Uploader:
             "links": [],
             "conversation_references": [],
         }
+        self.templates: dict[str, str] = {
+            "authors": "(id, name, username, description, followers_count, following_count, tweet_count, listed_count)",
+            "conversations": "(id, author_id, content, possibly_sensitive, language, source, retweet_count, reply_count, like_count, quote_count, created_at)",
+            "hashtags": "(tag)",
+            "conversation_hashtags": "(conversation_id, hashtag_id)",
+            "context_domains": "(id, name, description)",
+            "context_entities": "(id, name, description)",
+            "context_annotations": "(conversation_id, context_domain_id, context_entity_id)",
+            "annotations": "(conversation_id, value, type, probability)",
+            "links": "(conversation_id, url, title, description)",
+            "conversation_references": "(conversation_id, parent_id, type)",
+        }
 
     def upload(self, *args):
         raise NotImplementedError
@@ -56,7 +59,7 @@ class Uploader:
                 datetime.now() - self.bt,
                 datetime.now() - self.last_time))
             self.last_time = datetime.now()
-            execute_values(self.cursor, "INSERT INTO {} VALUES %s ON CONFLICT DO NOTHING".format(table_name), self.buff[table_name])
+            execute_values(self.cursor, "INSERT INTO {}{} VALUES %s ON CONFLICT DO NOTHING".format(table_name, self.templates[table_name]), self.buff[table_name])
             self.conn.commit()
             self.buff[table_name] = []
 
@@ -80,36 +83,67 @@ class AuthorsUploader(Uploader):
 class ConversationsUploader(Uploader):
 
     def upload(self, json_data: dict):
-        self.buff["context_domains"].append((
-
+        self.buff["authors"].append((
+            json_data["id"], json_data["author_id"], json_data["text"], json_data["possibly_sensitive"],
+            json_data["lang"], json_data["source"], json_data["public_metrics"]["retweet_count"],
+            json_data["public_metrics"]["reply_count"], json_data["public_metrics"]["like_count"],
+            json_data["public_metrics"]["quote_count"], json_data["created_at"]
         ))
 
+        self._do_upload("conversations")
+        self.__upload_hashtags(json_data)
+        self.__upload_context_annotations(json_data)
+        self.__upload_annotations(json_data)
+        self.__upload_links(json_data)
+        self.__upload_references(json_data)
 
-        self._do_upload("authors")
+    def __upload_hashtags(self, json_data: dict):
+        # annotations
+        for item in json_data["entities"].get("hashtags", []):
+            self.cursor.execute("""SELECT id FROM hashtags WHERE tag=%s""", [item["tag"]])
 
+            tag_id = self.cursor.fetchone()[0]
+
+            if tag_id is None:
+                self.cursor.execute("""INSERT INTO hashtags(tag) VALUES (%s) RETURNING ID""", [item["tag"]])
+
+            tag_id = self.cursor.fetchone()[0]
+
+            self.buff["conversation_hashtags"].append((
+                json_data["id"], tag_id
+            ))
+
+            self._do_upload("conversation_hashtags")
 
     def __upload_context_annotations(self, json_data: dict):
         # annotations
-        for item in json_data["entities"].get("annotations", []):
-            self.buff["links"].append((
-                None, json_data["conversation_id"], item["normalized_text"], item["type"], item["probability"]
+        for item in json_data.get("context_annotations", []):
+            self.buff["context_annotations"].append((
+                json_data["id"], item["domain"]["id"], item["entity"]["id"]
             ))
-            self._do_upload("annotations")
+            self.buff["context_domains"].append((
+                item["domain"]["id"], item["domain"]["name"], item["domain"]["description"]
+            ))
+            self.buff["context_entities"].append((
+                item["entity"]["id"], item["entity"]["name"], item["entity"]["description"]
+            ))
+            self._do_upload("context_annotations")
+            self._do_upload("context_domains")
+            self._do_upload("context_entities")
 
     def __upload_annotations(self, json_data: dict):
         # annotations
         for item in json_data["entities"].get("annotations", []):
-            self.buff["links"].append((
-                None, json_data["conversation_id"], item["normalized_text"], item["type"], item["probability"]
+            self.buff["annotations"].append((
+                json_data["id"], item["normalized_text"], item["type"], item["probability"]
             ))
             self._do_upload("annotations")
-
 
     def __upload_links(self, json_data: dict):
         # links
         for item in json_data["entities"].get("urls", []):
             self.buff["links"].append((
-                None, json_data["conversation_id"], item["expanded_url"], item["title"], item["description"]
+                json_data["id"], item["expanded_url"], item["title"], item["description"]
             ))
             self._do_upload("links")
 
@@ -117,14 +151,21 @@ class ConversationsUploader(Uploader):
         # conversation_references
         for item in json_data.get("referenced_tweets", []):
             self.buff["conversation_references"].append((
-                json_data["conversation_id"], item["id"], item["type"]
+                json_data["id"], item["id"], item["type"]
             ))
         self._do_upload("conversation_references")
 
     def close(self):
-        if len(self.buff["authors"]) > 0:
-            self._do_upload("authors", force=True)
+        self._do_upload("conversations", force=True)
+        self._do_upload("links", force=True)
+        self._do_upload("annotations", force=True)
+        self._do_upload("context_annotations", force=True)
+        self._do_upload("context_domains", force=True)
+        self._do_upload("context_entities", force=True)
+        self._do_upload("conversation_hashtags", force=True)
+        self._do_upload("conversation_references", force=True)
         super().close()
+
 
 def main():
     """
@@ -139,7 +180,7 @@ def main():
     with gzip.open("D:\\Downloads\\conversations.jsonl.gz", 'rt') as f:
         for line in f:
             json_data = orjson.loads(line.replace("\\u0000", ""))  # need to replace this because postgres wont accept \x00 cahracter
-            #uploader.upload(json_data)
+            uploader.upload(json_data)
             print(json_data)
             break
     uploader.close()
